1c613d5f7fafbaafcd28b02a6e13f212f8e77b80
Crisis19 <sebascristancho2006@gmail.com>
2025-09-27 07:19:40 -0500

    login y singup

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Login, Singup} from "./modules/login/pages"; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hola mundo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <input placeholder='escribe algo aqui'/>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Rutas públicas */}
+        <Route path="/" element={<Login />} />
+        <Route path="/Singup" element={<Singup />} />           
+        
+        {/* Ruta 404 */}
+        <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/modules/login/components/index.js
@@ -0,0 +1,5 @@
+export * from "./Login_form.jsx";
+export * from "./Email_form.jsx";
+export * from "./Pasword_form.jsx"; 
+export * from "./Singup_form.jsx";  
+export * from "./Verificar.jsx";
