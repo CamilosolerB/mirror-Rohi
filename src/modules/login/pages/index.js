5e2f78c98ee33193107bcfec51adfab231468f5e
CamilosolerB <caansobu2@gmail.com>
2025-09-30 22:06:27 -0500

    error pipeline

--- src/modules/login/pages/index.js
+++ src/modules/login/pages/index.js
@@ -1,5 +1,5 @@
 // Re-export only the named page components with correct casing.
-export { Login } from "./Login.jsx";
+export * from "./login.jsx";
 export { Singup } from "./Singup.jsx";
 export { Verificar } from "./Verificar.jsx";
 export { default as NewPassword } from "./New_password.jsx";
